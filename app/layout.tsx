1e5c84e2bc548194efdfdfe5f1e3d33de328053e
Recep Hakan Bektas <hakanbektas934@gmail.com>
2025-06-02 12:23:57 +0200

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import "./globals.css"
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "Nilay - Ma Petite Monde Rose 🐱",
@@ -16,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased">{children}</body>
+      <Analytics />
     </html>
   )
 }
